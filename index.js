d4eb9e76d39353863e56cadb988c54cf9d27abca
nadia <nadia.chayna.q@gmail.com>
2021-09-20 23:43:36 -0500

    functions path

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-module.exports = () => {
-  // ...
-};
+const path = require('path');
+const fs = require('fs');
+
+// ejemplos de rutas *absoluta & relativa*
+const absolutePath = 'C:\\Users\\Laboratoria\\Documents\\GitHub\\LIM015-md-links\\src\\archivos\\path.md';
+const relativePath = 'README.md'
+
+// console.log('hello')
+
+// Para ver si la ruta existe
+fs.access(absolutePath, (err) => {
+  console.log(`${absolutePath} ${err ? 'not exist' : 'exist'}`)
+})
+
+// Para ver si el path es absolute
+const isAbsolute = path.isAbsolute(absolutePath)
+console.log('Es absoluta', path.isAbsolute(absolutePath))
+
+// convertir una ruta relativa a absoluta
+function validatePath (relativePath) {
+  // operator ternario condiciones? expr1 :expr2
+  return path.isAbsolute(relativePath) === true ? relativePath : path.resolve(relativePath)
+}
+console.log(validatePath(absolutePath))
+
+// Funcion que verifica si existe la ruta
+function existsRoute (absolutePath) {
+  return fs.existsSync(absolutePath)
+}
+console.log(existsRoute(absolutePath))
+
+/* path.isAbsolute(relativePath, () => {
+  console.log(`${path.isAbsolute(relativePath) === true ? relativePath : path.resolve(relativePath)}`)
+}) */
