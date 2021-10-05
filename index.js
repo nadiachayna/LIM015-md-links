dc0cf7df94e53919caa0f90db01653e5a13748dd
nadia <nadia.chayna.q@gmail.com>
2021-10-05 09:20:46 -0500

    edición de  funciones

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { readFile } = require('fs/promises');
+const fetch = require ('node-fetch');
 const path = require('path');
 
 /* ************ Para ver si la ruta existe **************** */
@@ -31,16 +31,16 @@ const checkPath = (path) => {
   if ((statsObj.isFile() && extension(path))) {  //si es un archivo y es md
     array.push(path); 
   } else if (statsObj.isDirectory()) {   //si la ruta esta dentro de un directorio
-    const arrayPaths = readDirectory(path); 
+    const arrayPaths = readDirectory(path);
     arrayPaths.forEach(element => { 
-      const pathsDir = joinPaths(path,element); 
-      const savePaths = checkPath(pathsDir);    
+      const pathsDir = joinPaths(path,element);
+      const savePaths = checkPath(pathsDir);
       array = array.concat(savePaths);
     });
   }
   return array;
 };
-checkPath('Directory');
+//checkPath('Directory');
 //console.log(checkPath('Directory'));
 
 /* *********************** RegEx ************************ */
@@ -49,15 +49,15 @@ const regExText = /\[[^\s]+(.+?)\]/gi;
 const regExLink = /\((https?.+?)\)/gi;
 
 /* ************Función para extraer los links************ */
-  const getLinks = (route) => {
+  const getAllLinks = (route) => {
     const allLinks = fileContent(route).match(regEx);
     //console.log(allLinks, 'xd')
     const hereLinks = [];
     if(allLinks !== null) { 
       allLinks.forEach((link) => {
         const objectLinks = {
-         hrefLinks : link.match(regExLink).join().slice(1,-1),
-         textLinks : link.match(regExText).join().slice(1,-1),
+         href : link.match(regExLink).join().slice(1,-1),
+         text : link.match(regExText).join().slice(1,-1),
          file: route,
       };
       hereLinks.push(objectLinks);
@@ -66,10 +66,44 @@ const regExLink = /\((https?.+?)\)/gi;
     };
     return hereLinks;
   };
-  getLinks('Directory\\file1\\readme2.md')
-  console.log(getLinks('Directory\\file1\\readme2.md'))
+  const demo = getAllLinks('Directory\\file1\\readme2.md')
+  //console.log(getAllLinks('Directory\\file1\\readme2.md'))
 
-  
+/* ***************Función para validar****************** */
+const validateLinks = (arrayLink) => {
+  const arrStatus = arrayLink.map((link) => {
+      return fetch(link.href)
+      .then((result) => {
+          const statusText = result.status === 200 ? 'Ok' : 'Fail';
+          const data = {
+              file: link.file,
+              href: link.href,
+              message: statusText,
+              text: (link.text.slice(0, 50)), // Para limitar el texto a 50 caracteres
+              status: result.status,
+          };
+          return data;
+      })
+      .catch((error) => {
+          const data = {
+              href: link.href,
+              status: 'No status',
+              file: link.file,
+              message: `Fail ${error.message}`,
+          };
+          return data;
+      });
+  });
+  return Promise.all(arrStatus)
+  /*.then((res) => {
+      console.log(res)
+  })
+  .catch((err) => {
+      console.log(err)
+  })*/
+};
+//validateLinks(demo);
+
 module.exports = {
   existPath,
   isAbsolute,
@@ -79,5 +113,6 @@ module.exports = {
   fileContent,
   joinPaths,
   checkPath,
-  getLinks
+  getAllLinks, 
+  validateLinks
 };
